903efecbad6a1a9bc173e214ff8c59efd9613d7c
ROBERTOCABALLEROCUBERO <91220443+ROBERTOCABALLEROCUBERO@users.noreply.github.com>
2022-11-14 03:10:27 +0100

    Cambios generales

--- resultados.php
+++ resultados.php
@@ -1,14 +1,42 @@
+<?php
 
+
+
+
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stylemenu.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 </head>
+
 <body>
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="cerrarsesion.php">Cerrar sesion</a>
+
+    </nav>
+
+    <div class="card" style="width: 40rem;" id="tarjeta">
+        <img src="<?php echo $image ?>" class="card-img-top" alt="Foto de perfil" style="width 30rem;">
+        <div class="card-body">
+            <h5 class="card-title">Bienvenido al juego</h5>
+            <p class="card-text">El juego es un quiz de futbol</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Nombre: <?php echo $nombre; ?></li>
+            <li class="list-group-item">Puntuacion máxima: <?php echo $valormax['Maxima']?> </li>
+        </ul>
+    </div>
 </body>
+
 </html>
